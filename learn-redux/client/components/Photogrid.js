bd2844fdec002fa5b19ae741ef4c43bed0860668
LevyPaul <Lcocarell2816@conestogac.on.ca>
2019-04-05 17:09:31 -0400

    adding action creators

---
+++ learn-redux/client/components/Photogrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Photogrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+               I am the photo grid  
+                    
+            </div>
+        )
+    }
+});
+
+export default Photogrid;
