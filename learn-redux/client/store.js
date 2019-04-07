65e7d25d585eefa9120be211c8a36e199d350c98
Lev Cocarell <levyprog@Levs-MacBook-Pro.local>
2019-04-07 13:23:01 -0400

    added support for chrom

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -14,8 +14,20 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+
+const enchancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enchancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot ) {
+  module.hot.accept('./reducers/', () => {
+  const nextRootReducer = require('./reducers/index').default;
+  store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
